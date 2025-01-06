99033d565921cdb3b33b8c080a115c5e16b45670
omar Abdulrahman <omarncc00@gmail.com>
2025-01-07 01:28:24 +0200

    Initial commit

---
+++ app/src/main/java/com/example/hospitalapp/ui/profile/ProfileViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.hospitalapp.ui.profile
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.hospitalapp.ApiServices
+import com.example.hospitalapp.SingleLiveEvent
+import com.example.hospitalapp.models.UserModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+@HiltViewModel
+class ProfileViewModel @Inject constructor(private val apiServices: ApiServices) : ViewModel() {
+
+    private val _profileLiveData = SingleLiveEvent<UserModel?>()
+    val profileLiveData get() = _profileLiveData
+
+    fun getProfile(id: Int) {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                val response = apiServices.showProfile(id)
+                if (response.status == 1) {
+                    _profileLiveData.postValue(response)
+                } else {
+                    _profileLiveData.postValue(null)
+                }
+            }catch (e: Exception){
+                _profileLiveData.postValue(null)
+            }
+
+        }
+
+    }
+}
